0a0761174852ceeaa824dc2bfeac84f987c8ae14
theambitiousone <elaizajeantiu@gmail.com>
2026-04-12 22:32:59 +0800

    Add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,9 +12,12 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Lance & Elaiza | Wedding",
   description: "Official wedding website of Lance and Elaiza",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
