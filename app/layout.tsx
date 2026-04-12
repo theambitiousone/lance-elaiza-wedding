4de2ec18e055d989770ceb56cde1f42512ced109
theambitiousone <elaizajeantiu@gmail.com>
2026-04-12 22:37:51 +0800

    Fix favicon location

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Lance & Elaiza | Wedding",
   description: "Official wedding website of Lance and Elaiza",
   icons: {
